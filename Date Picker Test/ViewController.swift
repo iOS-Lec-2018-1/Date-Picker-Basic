f235b576b4a3672455b08ef5c53c377503a9d767
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-11 23:56:26 +0900

    add timer and alert

--- Date Picker Test/ViewController.swift
+++ Date Picker Test/ViewController.swift
@@ -11,22 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var myDatePicker: UIDatePicker!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // initial Picker Time setting
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from: myDatePicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+    }
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        if currentTimeLabel.text == timeLabel.text {
+            view.backgroundColor = UIColor.red
+        }
     }
 
     @IBAction func changeDatePicker(_ sender: Any) {
-        print("change Date Picker")
-//        print(myDatePicker.date)
-        
         // dateFormat
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         timeLabel.text = formatter.string(from: myDatePicker.date)
-        
-//        timeLabel.text = String(describing: myDatePicker.date)
         
     }
 }
