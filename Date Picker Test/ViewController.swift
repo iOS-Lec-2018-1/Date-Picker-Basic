6f9603b6855cad0db1a9e669b4566bdf8c9ea245
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-15 23:37:24 +0900

    Add Alert Action

--- Date Picker Test/ViewController.swift
+++ Date Picker Test/ViewController.swift
@@ -36,6 +36,19 @@ class ViewController: UIViewController {
         
         if currentTimeLabel.text == timeLabel.text {
             view.backgroundColor = UIColor.red
+            
+            // Alert
+            let timeAlert = UIAlertController(title: "알림", message: "설정된 시간입니다!!!", preferredStyle: .alert)
+//            let okAction = UIAlertAction(title: "네", style: .default, handler: nil)
+            let okAction = UIAlertAction(title: "네", style: .default, handler: { (UIAlertAction) -> Void in
+                    self.view.backgroundColor = UIColor.white
+                })
+            
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            timeAlert.addAction(okAction)
+            timeAlert.addAction(cancelAction)
+            
+            present(timeAlert, animated: true, completion: nil)
         }
     }
 
